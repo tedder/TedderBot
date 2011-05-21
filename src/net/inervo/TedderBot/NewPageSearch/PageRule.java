6b3a09cb0d405809e6993e31ffbd27a56cf92c1f
Ted Timmons <ted@perljam.net>
2011-05-20 22:17:23 -0700

    fixed the wrong one

--- src/net/inervo/TedderBot/NewPageSearch/PageRule.java
+++ src/net/inervo/TedderBot/NewPageSearch/PageRule.java
@@ -285,7 +285,7 @@ public class PageRule {
 	 * @return
 	 */
 	public String getArchivePage() {
-		return getSearchResultPage() + "/archive";
+		return "User:TedderBot/NewPageSearch/" + searchName + "/archive";
 	}
 
 	/**
@@ -294,7 +294,7 @@ public class PageRule {
 	 * @return
 	 */
 	public String getOldArchivePage() {
-		return "User:AlexNewArtBot/" + searchName + "/archive";
+		return getSearchResultPage() + "/archive";
 	}
 
 	/**
